3e1c0887bd89386b3653f4fda5d0ca2a1ba8df7b
biccon <admin@idx.kr>
2016-05-30 18:27:00 -0700

    .

--- calculator.c
+++ calculator.c
@@ -8,7 +8,8 @@
 int main(){
 	char *temp;
 	temp = inputExpression();
-	//splitExpression(temp);
-	printf("%s\n", postfix(temp));
-	replaceExpression(temp);
+	printf("%d\n", ParenMatch(temp));
+	//assignExpression(temp);
+//	printf("%s\n", postfix(temp));
+	//replaceExpression(temp);
 }
